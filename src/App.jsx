ed4556c70b3a482ba91e5bfed9739d8296fe01f8
Vaibhaviii14 <vaibhavi.s147@gmail.com>
2025-09-07 15:35:33 +0530

    Fixed conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,6 @@ import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Intro from "./components/intro/Intro.jsx";
 import Navbar from "./components/navbar/Navbar.jsx";
 import Hero from "./components/hero/Hero.jsx";
-<<<<<<< HEAD
-import About from "./pages/home/AboutUs.jsx";
-import Events from "./pages/events/Events.jsx";
-import FAQ from "./pages/FAQ.jsx";
-
-export default function App() {
-  return (
-    <div className="relative min-h-screen overflow-x-hidden">
-      <Navbar />
-
-      <section id="home" className="min-h-screen">
-        <Hero />
-      </section>
-
-      <section id="about" className="min-h-screen">
-        <About />
-      </section>
-
-      <section id="faq" className="min-h-screen">
-        <FAQ />
-      </section>
-
-    <section id="events" className="min-h-screen">
-        <Events />
-      </section>
-    </div>
-  );
-}
-=======
 import Footer from "./components/footer/Footer.jsx";
 import BottomCTAs from "./components/hero/BottomCTAs.jsx";
 import RightRail from "./components/hero/RightRail.jsx";
@@ -72,4 +43,3 @@ export default function App() {
     </BrowserRouter>
   );
 }
->>>>>>> upstream/dev
