a7d5d63427a88449823900f3ba715095d4f69422
Shreyash Kumar Dheemar <techshreyash123@gmail.com>
2025-09-23 09:00:44 +0530

    Added Click Sound To Buttons/Anchor Tags

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import {React,useEffect} from "react";
+import { React, useEffect } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Intro from "./components/intro/Intro.jsx";
-import Lenis from "lenis"; 
+import Lenis from "lenis";
 import Home from "./pages/home/Home.jsx"
 import Navbar from "./components/navbar/Navbar.jsx";
 import Hero from "./components/hero/Hero.jsx";
@@ -9,33 +9,35 @@ import Footer from "./components/footer/Footer.jsx";
 import BottomCTAs from "./components/hero/BottomCTAs.jsx";
 import RightRail from "./components/hero/RightRail.jsx";
 import SocialRail from "./components/hero/SocialRail.jsx";
+import ClickSoundProvider from "./utils/ClickSoundProvider.jsx";
 
 
 export default function App() {
 
 
-  //smooth scroll using lenis
-  useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.2,
-      smooth: true,
-      smoothTouch: true, // enable smooth scroll on touchpads/laptops
-    });
+    //smooth scroll using lenis
+    useEffect(() => {
+        const lenis = new Lenis({
+            duration: 1.2,
+            smooth: true,
+            smoothTouch: true, // enable smooth scroll on touchpads/laptops
+        });
 
-    function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
+        function raf(time) {
+            lenis.raf(time);
+            requestAnimationFrame(raf);
+        }
 
-    requestAnimationFrame(raf);
+        requestAnimationFrame(raf);
 
-    return () => lenis.destroy();
-  }, []);
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  );
+        return () => lenis.destroy();
+    }, []);
+    return (
+        <BrowserRouter>
+            <ClickSoundProvider />
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
